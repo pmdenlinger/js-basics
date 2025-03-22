65b0ab25c5a0a1c4243af69fcaa14604a03b3910
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-21 19:57:25 -0700

    Add const for defaultResult and string literal with backticks

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,7 +1,8 @@
+const defaultResult = 0;
 let currentResult = 0;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-let calculationDescription = "(0 + 10) * 3 / 2 - 1";
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
