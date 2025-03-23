36d0d2f879bec0de46ff20e4ec1eb96545c6ca35
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-23 10:48:00 -0700

    Add parseInt() method to add function to convert input into Int type

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,9 +2,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, '');
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, "");
 }
 
-addBtn.addEventListener('click', add);
-
+addBtn.addEventListener("click", add);
---
+++ basics-01-starting-project/basics-assignment-2-start/assignment.js
@@ -0,0 +1,10 @@
+const task3Element = document.getElementById("task-3");
+task3Element.addEventListener("click", alert);
+
+const task = alert("This is a JS alert!");
+
+function greetUser(name) {
+  alert("Hello, " + name + "!");
+}
+
+// greetUser("Max");
